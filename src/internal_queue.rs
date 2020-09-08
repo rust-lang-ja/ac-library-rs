83e4ec035f43b760f837a4068d57e35768f5be0a
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2020-09-09 01:20:33 +0900

    Change visibility

--- src/internal_queue.rs
+++ src/internal_queue.rs
@@ -1,38 +1,38 @@
-struct SimpleQueue<T> {
+pub(crate) struct SimpleQueue<T> {
     payload: Vec<T>,
     pos: usize,
 }
 
 impl<T> SimpleQueue<T> {
-    fn reserve(&mut self, n: usize) {
+    pub(crate) fn reserve(&mut self, n: usize) {
         if n > self.payload.len() {
             self.payload.reserve(n - self.payload.len());
         }
     }
 
-    fn size(&self) -> usize {
+    pub(crate) fn size(&self) -> usize {
         self.payload.len() - self.pos
     }
 
-    fn empty(&self) -> bool {
+    pub(crate) fn empty(&self) -> bool {
         self.pos == self.payload.len()
     }
 
-    fn push(&mut self, t: T) {
+    pub(crate) fn push(&mut self, t: T) {
         self.payload.push(t);
     }
 
     // Do we need mutable version?
-    fn front(&self) -> &T {
+    pub(crate) fn front(&self) -> &T {
         &self.payload[self.pos]
     }
 
-    fn clear(&mut self) {
+    pub(crate) fn clear(&mut self) {
         self.payload.clear();
         self.pos = 0;
     }
 
-    fn pop(&mut self) {
+    pub(crate) fn pop(&mut self) {
         self.pos += 1;
     }
 }
