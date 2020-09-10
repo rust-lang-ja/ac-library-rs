d78cdaf48a1001651a6f643a963fe45851b2cd2e
ukiyoyo <13175394+ukiyoyo@users.noreply.github.com>
2020-09-10 23:31:29 +0900

    Implement scc

--- src/scc.rs
+++ src/scc.rs
@@ -1,1 +1,25 @@
+use crate::internal_scc;
+
+pub struct SccGraph {
+    internal: internal_scc::SccGraph,
+}
+
+impl SccGraph {
+    pub fn new(n: usize) -> Self {
+        SccGraph {
+            internal: internal_scc::SccGraph::new(n),
+        }
+    }
+
+    pub fn add_edge(&mut self, from: usize, to: usize) {
+        let n = self.internal.num_vertices();
+        assert!(from < n);
+        assert!(to < n);
+        self.internal.add_edge(from, to);
+    }
+
+    pub fn scc(&self) -> Vec<Vec<usize>> {
+        self.internal.scc()
+    }
+}
 
