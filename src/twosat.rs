3bba0cab384a2340a93404d3ef8b2889b64c4219
manta1130 <mt1996@live.jp>
2020-09-12 09:03:46 +0900

    Add tests

--- src/twosat.rs
+++ src/twosat.rs
@@ -34,3 +34,75 @@ impl TwoSAT {
         &self.answer
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn solve_alpc_h_sample1() {
+        // https://atcoder.jp/contests/practice2/tasks/practice2_h
+
+        let (n, d) = (3, 2);
+        let x = [1, 2, 0i32];
+        let y = [4, 5, 6];
+
+        let mut t = TwoSAT::new(n);
+
+        for i in 0..n {
+            for j in i + 1..n {
+                if (x[i] - x[j]).abs() < d {
+                    t.add_clause(i, false, j, false);
+                }
+                if (x[i] - y[j]).abs() < d {
+                    t.add_clause(i, false, j, true);
+                }
+                if (y[i] - x[j]).abs() < d {
+                    t.add_clause(i, true, j, false);
+                }
+                if (y[i] - y[j]).abs() < d {
+                    t.add_clause(i, true, j, true);
+                }
+            }
+        }
+        assert!(t.satisfiable());
+        let answer = t.answer();
+        let mut res = vec![];
+        for (i, &v) in answer.iter().enumerate() {
+            if v {
+                res.push(x[i])
+            } else {
+                res.push(y[i]);
+            }
+        }
+        assert_eq!(res, [4, 2, 0]);
+    }
+
+    #[test]
+    fn solve_alpc_h_sample2() {
+        // https://atcoder.jp/contests/practice2/tasks/practice2_h
+
+        let (n, d) = (3, 3);
+        let x = [1, 2, 0i32];
+        let y = [4, 5, 6];
+
+        let mut t = TwoSAT::new(n);
+
+        for i in 0..n {
+            for j in i + 1..n {
+                if (x[i] - x[j]).abs() < d {
+                    t.add_clause(i, false, j, false);
+                }
+                if (x[i] - y[j]).abs() < d {
+                    t.add_clause(i, false, j, true);
+                }
+                if (y[i] - x[j]).abs() < d {
+                    t.add_clause(i, true, j, false);
+                }
+                if (y[i] - y[j]).abs() < d {
+                    t.add_clause(i, true, j, true);
+                }
+            }
+        }
+        assert!(!t.satisfiable());
+    }
+}
