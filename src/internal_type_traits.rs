d51a67bd7f2f34096bc80ae24f30f67f4a8ebb8b
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2020-09-14 23:15:51 +0900

    Split integral traits into four subtraits

--- src/internal_type_traits.rs
+++ src/internal_type_traits.rs
@@ -52,37 +52,65 @@ pub trait Integral:
     + fmt::Debug
     + fmt::Binary
     + fmt::Octal
+    + Zero
+    + One
+    + BoundedBelow
+    + BoundedAbove
 {
+}
+
+/// Class that has additive identity element
+pub trait Zero {
+    /// The additive identity element
     fn zero() -> Self;
+}
+
+/// Class that has multiplicative identity element
+pub trait One {
+    /// The multiplicative identity element
     fn one() -> Self;
+}
+
+pub trait BoundedBelow {
     fn min_value() -> Self;
+}
+
+pub trait BoundedAbove {
     fn max_value() -> Self;
 }
 
 macro_rules! impl_integral {
     ($($ty:ty),*) => {
         $(
-            impl Integral for $ty {
+            impl Zero for $ty {
                 #[inline]
                 fn zero() -> Self {
                     0
                 }
+            }
 
+            impl One for $ty {
                 #[inline]
                 fn one() -> Self {
                     1
                 }
+            }
 
+            impl BoundedBelow for $ty {
                 #[inline]
                 fn min_value() -> Self {
                     Self::min_value()
                 }
+            }
 
+            impl BoundedAbove for $ty {
                 #[inline]
                 fn max_value() -> Self {
                     Self::max_value()
                 }
             }
+
+            impl Integral for $ty {}
         )*
     };
 }
