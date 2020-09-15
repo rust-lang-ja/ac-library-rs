d8a81476ea5c77169ec6a6863fecd076ee6d401f
manta1130 <mt1996@live.jp>
2020-09-15 22:03:01 +0900

    Changed the test code

--- .github/workflows/test-expand.sh
+++ .github/workflows/test-expand.sh
@@ -7,13 +7,15 @@ TEST_FILE="test.rs"
 FILE_HEAD="fn main() {}"
 
 for MODULE in ${TEST_MODULES[@]};do
+     echo Test module $MODULE 
      python3 $SCRIPT_DIR/../../expand.py $MODULE > $TMP_PATH/$TEST_FILE
+     echo "Output" $(wc -c < $TMP_PATH/$TEST_FILE) "Bytes"
      echo $FILE_HEAD >> $TMP_PATH/$TEST_FILE
      rustc $TMP_PATH/$TEST_FILE 2>/dev/null
      if [ $? -ne 0 ];then
         echo "Error compiling for $MODULE"
         exit 1
     else
-        echo "Test passed($MODULE)"
+        echo "Test passed"
      fi
 done
