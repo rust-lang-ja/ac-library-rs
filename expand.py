9760ead7edceb01b159034268a0d5ba618e40c84
manta1130 <mt1996@live.jp>
2020-09-16 19:15:48 +0900

    Formatted codes

--- expand.py
+++ expand.py
@@ -24,15 +24,24 @@ You can select multiple modules for <output modules>
     e.g.)expand.py math segtree
 
 Options:
-    -a  --all       import all modules   
+    -a  --all       import all modules
     -h  --help      print help
 '''
 output_header = '//https://github.com/rust-lang-ja/ac-library-rs\n'
 opt_list = ['help', 'all']
 output_list_all = ('lazysegtree', 'segtree', 'convolution', 'twosat', 'scc',
-                   'fenwicktree', 'math', 'modint', 'maxflow', 'dsu', 'mincostflow', 'string', 'internal_bit', 'internal_math', 'internal_type_traits', 'internal_scc', 'internal_queue')
-dependency_list = {'lazysegtree': ('internal_bit',), 'segtree': ('internal_bit',), 'convolution': ('internal_bit', 'modint',), 'math': ('internal_math',), 'modint': (
-    'internal_math', 'internal_type_traits'), 'fenwicktree': ('internal_type_traits',), 'twosat': ('internal_scc',), 'scc': ('internal_scc',), 'maxflow': ('internal_queue', 'internal_type_traits',), 'mincostflow': ('internal_type_traits',)}
+                   'fenwicktree', 'math', 'modint', 'maxflow', 'dsu',
+                   'mincostflow', 'string', 'internal_bit', 'internal_math',
+                   'internal_type_traits', 'internal_scc', 'internal_queue')
+dependency_list = {'lazysegtree': ('internal_bit',),
+                   'segtree': ('internal_bit',),
+                   'convolution': ('internal_bit', 'modint',),
+                   'math': ('internal_math',),
+                   'modint': ('internal_math', 'internal_type_traits'),
+                   'fenwicktree': ('internal_type_traits',),
+                   'twosat': ('internal_scc',), 'scc': ('internal_scc',),
+                   'maxflow': ('internal_queue', 'internal_type_traits',),
+                   'mincostflow': ('internal_type_traits',)}
 src_path = 'src/'
 
 
@@ -72,7 +81,7 @@ output_list = set()
 
 while len(args) != 0:
     pop = args.pop()
-    if not pop in output_list_all:
+    if pop not in output_list_all:
         print('invalid args:{}'.format(pop))
         print(usage)
         sys.exit(2)
@@ -90,7 +99,8 @@ for i in output_list:
     output_data.extend(buf)
 
 for i in output_list:
-    # Modules that begin with 'internal' are for internal use, so they are not declared.
+    # Modules that begin with 'internal' are for internal use, so they are not
+    # declared.
     if not i.startswith('internal'):
         output_data.append('use {}::*;'.format(i))
 
