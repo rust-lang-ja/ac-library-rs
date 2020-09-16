e52eaf4393a0143f85db234c34f90897db095f4b
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2020-09-16 13:56:43 +0900

    Add a sample code for G - SCC

---
+++ examples/practice2_g_scc.rs
@@ -0,0 +1,26 @@
+use ac_library_rs::SccGraph;
+use std::io::Read;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+    let mut input = buf.split_whitespace();
+
+    let n = input.next().unwrap().parse().unwrap();
+    let m = input.next().unwrap().parse().unwrap();
+    let mut graph = SccGraph::new(n);
+    for _ in 0..m {
+        let a = input.next().unwrap().parse().unwrap();
+        let b = input.next().unwrap().parse().unwrap();
+        graph.add_edge(a, b);
+    }
+    let scc = graph.scc();
+    println!("{}", scc.len());
+    for cc in scc {
+        print!("{}", cc.len());
+        for v in cc {
+            print!(" {}", v);
+        }
+        println!();
+    }
+}
