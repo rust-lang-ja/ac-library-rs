fcacbc1981073d144b682c67e47f80ff4a694061
manta1130 <46486762+manta1130@users.noreply.github.com>
2020-09-17 08:16:47 +0900

    Remove unnecessary space
    Co-authored-by: Ryo Yamashita <qryxip@gmail.com>

--- .github/workflows/test-expand.sh
+++ .github/workflows/test-expand.sh
@@ -7,11 +7,11 @@ TEST_FILE="test.rs"
 FILE_HEAD="fn main() {}"
 
 for MODULE in ${TEST_MODULES[@]};do
-     echo Test module $MODULE 
+     echo Test module $MODULE
      python3 $SCRIPT_DIR/../../expand.py $MODULE > $TMP_PATH/$TEST_FILE
      echo "Output" $(wc -c < $TMP_PATH/$TEST_FILE) "Bytes"
      echo $FILE_HEAD >> $TMP_PATH/$TEST_FILE
-     rustc -A warnings $TMP_PATH/$TEST_FILE 
+     rustc -A warnings $TMP_PATH/$TEST_FILE
      if [ $? -ne 0 ];then
         echo "Error compiling for $MODULE"
         exit 1
