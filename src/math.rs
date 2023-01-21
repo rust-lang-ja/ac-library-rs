5d41c79fade1238e9130e5fc15ed2d7db1d8bfdf
Mizar <mizar.jp@gmail.com>
2023-01-21 17:04:38 +0900

    Improve floor_sum https://github.com/atcoder/ac-library/pull/88

--- src/math.rs
+++ src/math.rs
@@ -188,7 +188,7 @@ pub fn crt(r: &[i64], m: &[i64]) -> (i64, i64) {
 pub fn floor_sum(n: i64, m: i64, mut a: i64, mut b: i64) -> i64 {
     let mut ans = 0;
     if a >= m {
-        ans += (n - 1) * n * (a / m) / 2;
+        ans += (n - 1) * n / 2 * (a / m);
         a %= m;
     }
     if b >= m {
@@ -196,13 +196,11 @@ pub fn floor_sum(n: i64, m: i64, mut a: i64, mut b: i64) -> i64 {
         b %= m;
     }
 
-    let y_max = (a * n + b) / m;
-    let x_max = y_max * m - b;
-    if y_max == 0 {
+    let y_max = a * n + b;
+    if y_max < m {
         return ans;
     }
-    ans += (n - (x_max + a - 1) / a) * y_max;
-    ans += floor_sum(y_max, a, m, (a - x_max % a) % a);
+    ans += floor_sum(y_max / m, a, m, y_max % m);
     ans
 }
 
